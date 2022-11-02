946e0d8da9ce41041ceef0cb4e66120436f964f1
CRISPYTYPER <jack3409@naver.com>
2022-11-02 16:57:34 +0900

    Add UDPClient, UDPServer classes

---
+++ src/UDPClient.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UDPClient {
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress IPAddress = InetAddress.getByName("127.0.0.1");
+        byte[] sendData = new byte[1024];
+        byte[] receiveData = new byte[1024];
+
+//        한문장 읽기
+        String sentence = inFromUser.readLine();
+//        byte로 변환
+        sendData = sentence.getBytes();
+
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 8001);
+
+        clientSocket.send(sendPacket);
+
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+        clientSocket.receive(receivePacket);
+
+        String modifiedSentence = new String(receivePacket.getData());
+
+        System.out.println("FROM SERVER:" + modifiedSentence);
+        clientSocket.close();
+    }
+}
---
+++ src/UDPServer.java
@@ -0,0 +1,33 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UDPServer {
+
+    public static void main(String args[]) throws Exception {
+
+        DatagramSocket serverSocket = new DatagramSocket(8001);
+
+        byte[] receiveData = new byte[1024];
+        byte[] sendData = new byte[1024];
+
+        while(true) {
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length); // Create space for received datagram
+            serverSocket.receive(receivePacket); // Receive Datagram
+
+            String sentence = new String(receivePacket.getData());
+
+            InetAddress IPAddress = receivePacket.getAddress();
+
+            int port = receivePacket.getPort();
+
+            String capitalizedSentence = sentence.toUpperCase() + '\0';
+
+            sendData = capitalizedSentence.getBytes();
+
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+
+            serverSocket.send(sendPacket);
+        }
+    }
+}
