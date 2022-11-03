4fab36a4e72fb6dcd6a5b09191eda51b463e0174
CRISPYTYPER <jack3409@naver.com>
2022-11-03 13:25:07 +0900

    add codes to get multicastAddress

--- src/Peer.java
+++ src/Peer.java
@@ -1,22 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Peer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
         int portNum = 0;
-        String inputStr;
-        Scanner scan = new Scanner(System.in);
+        boolean isCommand = false;
+        String commandPhrase = "";
 
-        if(args.length != 1) {
-            System.out.println("Port 번호를 입력해주세요.");
+        if (args.length != 1) {
+            System.out.println("Port 번호를 올바르게 입력해주세요.");
             System.exit(0);
         } else {
             portNum = Integer.parseInt(args[0]);
         }
+//        System.out.println(portNum);
 
-        while(true) {
-            inputStr = scan.nextLine();
-            char firstCharacter = inputStr.charAt(0);
-            System.out.println(firstCharacter);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String inputString;
+        while ((inputString = reader.readLine()) != null) {
+//            System.out.println(inputString);
+//        System.out.println(inputString.charAt(0));
+//            아무 입력 안받으면 다시 입력 받기
+            if (inputString.length() == 0) {
+                continue;
+            }
+            String[] splitedInput = inputString.split(" ");
+//            System.out.println(splitedInput.length);
+//        System.out.println(Arrays.toString(splitedInput));
+//            '#'확인되면 isCommand flag true로 바꾸기
+            isCommand = (splitedInput[0].charAt(0) == '#') ? true : false;
+//            System.out.println(isCommand);
+            if(isCommand == true) {
+//                commandPhrase는 #지운 명령어 자체(e.g. #JOIN -> JOIN)
+                commandPhrase = splitedInput[0].substring(1);
+//                System.out.println(commandPhrase);
+                switch (commandPhrase) {
+                    case "JOIN" :
+                        // TODO JOIN 입력 받았을 때 처리하기
+                        // 인풋 다음과 같이 받음. #JOIN (참여할 채팅방의 이름) (사용자 이름)
+                        if(splitedInput.length != 3) {
+                            System.out.println("ERROR! 다음과 같은 형식으로 입력해주세요.");
+                            System.out.println("#JOIN (참여할 채팅방의 이름) (사용자 이름)");
+                            continue;
+                        }
+                        SHA256 sha256 = new SHA256();
+                        String multicastAddress = sha256.getMulticastAddress(splitedInput[1]);
+                        System.out.println(multicastAddress);
+
+                }
+            }
         }
+
+
     }
 }
